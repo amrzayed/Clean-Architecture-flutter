0321c56b5cdf85ac32e5a0bc8a066cc52f8c803e
amr zayed <aabdelhalim@ipmagix.com>
2022-07-25 15:12:23 +0200

    Bloc Implementation

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -2,26 +2,43 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:learn/core/constats/strings.dart';
+import 'package:learn/core/util/input_converter.dart';
 import 'package:learn/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:learn/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:learn/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 
 part 'number_trivia_event.dart';
 
 part 'number_trivia_state.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc() : super(NumberTriviaInitial()) {
-    on<NumberTriviaEvent>((event, emit) {
-      // TODO: implement event handler
-    });
-  }
+
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc({
+    required this.getConcreteNumberTrivia,
+    required this.getRandomNumberTrivia,
+    required this.inputConverter,
+  });
 
   @override
   // TODO: implement initialState
-  NumberTriviaState get initialState => throw UnimplementedError();
+  NumberTriviaState get initialState => Empty();
 
   @override
-  Stream<NumberTriviaState> mapEventToState(NumberTriviaEvent event) {
-    // TODO: implement mapEventToState
-    throw UnimplementedError();
+  Stream<NumberTriviaState> mapEventToState(NumberTriviaEvent event) async* {
+    if (event is GetTriviaForConcreteNumber) {
+      final inputEither =
+          inputConverter.stringToUnsignedIntegers(event.numberString);
+      yield* inputEither.fold(
+        (failure) async* {
+          yield Error(message: INVALID_INPUT_FAILURE_MESSAGE);
+        },
+        (integer) => throw UnimplementedError(),
+      );
+    }
   }
 }
